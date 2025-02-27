9b22a32d799b61ed147e91e175133323fb998881
Sumit99089 <sumitlaha47@gmail.com>
2025-02-28 02:30:54 +0530

    Work done, item added to bottom nav

--- app/src/main/java/com/nshm/attendancesystem/MainActivity.kt
+++ app/src/main/java/com/nshm/attendancesystem/MainActivity.kt
@@ -3,13 +3,30 @@ package com.nshm.attendancesystem
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Create
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Warning
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import kotlinx.coroutines.flow.MutableStateFlow
 
@@ -27,24 +44,24 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MyApp() {
     val navController = rememberNavController()
-    val currentScreen = remember { MutableStateFlow(NavigationDestination.Scan) }
+    val currentDestination = navController.currentBackStackEntryAsState()
+    val currentScreenTitle = remember { mutableStateOf("Scan") }
+
+    // Update the title whenever navigation changes
+    LaunchedEffect(currentDestination.value?.destination?.route) {
+        currentDestination.value?.destination?.route?.let { route ->
+            currentScreenTitle.value = route
+        }
+    }
 
     Scaffold(
         topBar = {
-            AnimatedTopBar(title = currentScreen.collectAsState().value.name)
+            AnimatedTopBar(title = currentScreenTitle.value)
         },
         bottomBar = {
             BottomNavigationBar(
                 navController = navController,
-                onTitleChange = {
-
-                    currentScreen.value = when (it) {
-                        "Scan" -> NavigationDestination.Scan
-                        "Register" -> NavigationDestination.Register
-                        "Attendance" -> NavigationDestination.Attendance
-                        else -> NavigationDestination.Scan
-                    }
-                }
+                currentRoute = currentDestination.value?.destination?.route
             )
         }
     ) { innerPadding ->
@@ -54,23 +71,77 @@ fun MyApp() {
             startDestination = NavigationDestination.Scan.name
         ) {
             composable(NavigationDestination.Scan.name) {
-                LaunchedEffect(key1 = Unit) {
-                    currentScreen.value = NavigationDestination.Scan
-                }
                 CameraScreen(navController = navController)
             }
             composable(NavigationDestination.Register.name) {
-                LaunchedEffect(key1 = Unit) {
-                    currentScreen.value = NavigationDestination.Register
-                }
                 RegisterScreen()
             }
             composable(NavigationDestination.Attendance.name) {
-                LaunchedEffect(key1 = Unit) {
-                    currentScreen.value = NavigationDestination.Attendance
-                }
                 AttendanceScreen()
             }
+            composable(NavigationDestination.Registered.name) {
+                RegisteredStudentsScreen()
+            }
+
+        }
+    }
+}
+
+@Composable
+fun BottomNavigationBar(
+    navController: NavController,
+    currentRoute: String?
+) {
+    val items = listOf(
+        NavigationDestination.Scan.name,
+        NavigationDestination.Register.name,
+        NavigationDestination.Attendance.name,
+        NavigationDestination.Registered.name
+    )
+
+    NavigationBar(
+        modifier = Modifier.height(80.dp),
+        containerColor = MaterialTheme.colorScheme.primaryContainer
+    ) {
+        items.forEach { screen ->
+            val icon = when (screen) {
+                NavigationDestination.Scan.name -> Icons.Default.Search
+                NavigationDestination.Register.name -> Icons.Default.Create
+                NavigationDestination.Attendance.name -> Icons.Default.Person
+                NavigationDestination.Registered.name-> Icons.Default.Face
+                else -> Icons.Default.Warning
+            }
+
+            NavigationBarItem(
+                modifier = Modifier.padding(2.dp),
+                icon = {
+                    Icon(
+                        imageVector = icon,
+                        contentDescription = screen,
+                        modifier = Modifier.size(24.dp),
+                        tint = if (currentRoute == screen) MaterialTheme.colorScheme.primary
+                        else MaterialTheme.colorScheme.onSurface
+                    )
+                },
+                label = {
+                    Text(
+                        text = screen,
+                        color = if (currentRoute == screen) MaterialTheme.colorScheme.primary
+                        else MaterialTheme.colorScheme.onSurface,
+                        style = if (currentRoute == screen) MaterialTheme.typography.bodyLarge
+                        else MaterialTheme.typography.bodyMedium
+                    )
+                },
+                selected = currentRoute == screen,
+                interactionSource = remember { MutableInteractionSource() },
+                onClick = {
+                    navController.navigate(screen) {
+                        popUpTo(navController.graph.startDestinationId) { saveState = true }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                }
+            )
         }
     }
 }
---
+++ app/src/main/java/com/nshm/attendancesystem/RegisteredStudentsScreen.kt
@@ -0,0 +1,238 @@
+package com.nshm.attendancesystem
+
+import android.widget.Toast
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import kotlinx.coroutines.delay
+import java.util.Locale
+
+@Composable
+fun RegisteredStudentsScreen(attendanceViewModel: AttendanceViewModel = viewModel()) {
+    val context = LocalContext.current
+    val keyboardController = LocalSoftwareKeyboardController.current
+
+    val userList by attendanceViewModel::registeredStudentsList
+    var isRefreshing by remember { mutableStateOf(true) }
+    var searchQuery by remember { mutableStateOf("") }
+    var selectedDepartment by remember { mutableStateOf("ALL") }
+    var isDropdownExpanded by remember { mutableStateOf(false) }
+    val departmentNames = listOf("ALL", "CSE", "AIML", "DS", "ECE", "BCA", "MCA", "BBA", "ME", "Civil")
+    var attendanceCount = 0
+
+    userList.forEach { user ->
+        if ( selectedDepartment == "ALL" && user.isSeminarAttendee) {
+            attendanceCount++
+        }
+        else if ( user.department == selectedDepartment && user.isSeminarAttendee ) {
+            attendanceCount++
+        }
+    }
+
+    LaunchedEffect(isRefreshing) {
+        if (isRefreshing) {
+            Toast.makeText(context, "Refreshing Data", Toast.LENGTH_SHORT).show()
+            attendanceViewModel.fetchStudentsList()
+            delay(1200)
+            isRefreshing = false
+        }
+    }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                Brush.verticalGradient(
+                    colors = listOf(
+                        Color(0xFFE3F2FD), Color(0xFFBBDEFB)
+                    )
+                )
+            )
+    ) {
+        Column {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(6.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                OutlinedTextField(
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(8.dp),
+                    value = searchQuery,
+                    onValueChange = { query -> searchQuery = query },
+                    placeholder = { Text("Search by College ID") },
+                    shape = MaterialTheme.shapes.medium,
+                    keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Phone, imeAction = ImeAction.Done),
+                    keyboardActions = KeyboardActions(onDone = { keyboardController?.hide()
+                    }),
+                    singleLine = true
+                )
+                Spacer(modifier = Modifier.width(2.dp))
+
+                Column(modifier = Modifier.wrapContentWidth()) {
+                    Text(
+                        text = attendanceCount.toString(),
+                        style = MaterialTheme.typography.bodySmall.copy(
+                            color = MaterialTheme.colorScheme.onSurfaceVariant
+                        )
+                    )
+                    Text(
+                        text = "Entries",
+                        style = MaterialTheme.typography.bodySmall.copy(
+                            color = MaterialTheme.colorScheme.onSurfaceVariant
+                        )
+                    )
+                }
+                Spacer(modifier = Modifier.width(2.dp))
+
+                Box {
+                    IconButton(onClick = { isDropdownExpanded = true }) {
+                        Icon(
+                            painter = painterResource(R.drawable.filter),
+                            contentDescription = "Filter"
+                        )
+                    }
+                    DropdownMenu(
+                        expanded = isDropdownExpanded,
+                        onDismissRequest = { isDropdownExpanded = false }
+                    ) {
+                        departmentNames.forEach { department ->
+                            DropdownMenuItem(
+                                text = { Text(department) },
+                                onClick = {
+                                    selectedDepartment = department
+                                    isDropdownExpanded = false
+                                }
+                            )
+                        }
+                    }
+                }
+                Spacer(modifier = Modifier.width(2.dp))
+
+                IconButton(onClick = { isRefreshing = true }) {
+                    Icon(Icons.Default.Refresh, contentDescription = "Refresh")
+                }
+            }
+
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(horizontal = 16.dp)
+            ) {
+                val filteredUsers = userList.filter { user ->
+                    val matchesDepartment = selectedDepartment == "ALL" || user.department == selectedDepartment
+                    val matchesSearchQuery = if ( searchQuery.isBlank() ) true
+                    else {
+                        try {
+                            val idToSearch = searchQuery.toLong()
+                            user.collegeId.toString().contains(idToSearch.toString())
+                        } catch (_: NumberFormatException) {
+                            false
+                        }
+                    }
+                    matchesDepartment && matchesSearchQuery
+                }
+
+                if (filteredUsers.isEmpty()) {
+                    item {
+                        Box(
+                            modifier = Modifier.fillMaxSize(),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Text(
+                                "No users found.",
+                                style = MaterialTheme.typography.bodyLarge.copy(
+                                    color = MaterialTheme.colorScheme.primary,
+                                    fontWeight = FontWeight.Medium
+                                )
+                            )
+                        }
+                    }
+                } else {
+                    items(filteredUsers) { user ->
+                        RegisteredUserInfoCard(user)
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun RegisteredUserInfoCard(user: User) {
+    if (user.isPresent) {
+        Card(
+            modifier = Modifier
+                .padding(vertical = 8.dp, horizontal = 16.dp)
+                .fillMaxWidth(),
+            elevation = CardDefaults.cardElevation(4.dp),
+            shape = RoundedCornerShape(12.dp),
+            colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surface)
+        ) {
+            Column(
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Text(
+                    text = user.name,
+                    style = MaterialTheme.typography.titleMedium.copy(
+                        fontWeight = FontWeight.Bold,
+                        color = MaterialTheme.colorScheme.onSurface
+                    )
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+
+                Row {
+                    Text(
+                        text = "College ID: ${user.collegeId}",
+                        style = MaterialTheme.typography.bodyMedium.copy(
+                            color = MaterialTheme.colorScheme.onSurfaceVariant
+                        )
+                    )
+                    Spacer(modifier = Modifier.width(8.dp))
+
+                    Text(
+                        text = " [ ${user.department.uppercase(Locale.ROOT)} - ${user.year.lowercase(Locale.ROOT)} ]",
+                        style = MaterialTheme.typography.bodyMedium.copy(
+                            fontStyle = FontStyle.Italic,
+                            color = MaterialTheme.colorScheme.onSurfaceVariant
+                        )
+                    )
+                }
+                Spacer(modifier = Modifier.height(8.dp))
+
+                Row{
+                    Text(
+                        text = user.collegeEmail.lowercase(Locale.ROOT),
+                        style = MaterialTheme.typography.bodySmall.copy(
+                            fontStyle = FontStyle.Italic,
+                            color = MaterialTheme.colorScheme.onSurfaceVariant
+                        )
+                    )
+                }
+            }
+        }
+    }
+}
